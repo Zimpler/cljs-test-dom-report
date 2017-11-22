84f0275f8caf2ac4f4cfacbe6addddb99d4b5ae7
Jell <jean-louis@jawaninja.com>
2017-11-22 07:51:42 +0100

    Fix clojar deets

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.zimpler/cljs-test-dom-report "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A cljs.test report that outputs in your browser. Good on its own but perfect in combination with figwheel."
+  :url "https://github.com/Zimpler/cljs-test-dom-report"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
